7603dbbae024401465b3764bc281dbc8f7560af0
Danika Gupta <danika@gprof.com>
2023-11-04 15:43:37 -0700

    Initial Commit

---
+++ ChatV02/ChatV02App.swift
@@ -0,0 +1,17 @@
+//
+//  ChatV02App.swift
+//  ChatV02
+//
+//  Created by Amit Gupta on 11/4/23.
+//
+
+import SwiftUI
+
+@main
+struct ChatV02App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
