7acc700137a8b50ed28b0ad574dc8aa3e5080647
Danika Gupta <danika@gprof.com>
2023-11-04 17:19:57 -0700

    Initial commit.

--- ChatV02/ChatV02App.swift
+++ ChatV02/ChatV02App.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct ChatV02App: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            MultiTabView()
         }
     }
 }
--- ChatV02/ContentView.swift
+++ ChatV02/ContentView.swift
@@ -7,18 +7,51 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct MultiTabView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        TabView{
+            Tab1View()
+                .tabItem{
+                    Label("Web page",systemImage: "eyes.inverse")
+                }
+            ChatGPTView(vm: ViewModel(api: ChatGPTAPI(apiKey: "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")))
+                .tabItem{
+                    Label("Chat",systemImage: "list.number")
+                }
+            Tab3View()
+                .tabItem{
+                    Label("Tab 3",systemImage: "keyboard")
+                }
         }
-        .padding()
     }
 }
 
-#Preview {
-    ContentView()
+struct Tab1View: View {
+    var body: some View {
+        Text("Page 1")
+            .font(.largeTitle)
+            .multilineTextAlignment(.center)
+            .edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+    }
 }
+
+struct Tab2View: View {
+    var body: some View {
+        Text("Page 2")
+    }
+}
+
+struct Tab3View: View {
+    var body: some View {
+        Text("Page 3")
+    }
+}
+
+struct MultiTabView_Previews: PreviewProvider {
+    static var previews: some View {
+        ZStack {
+            MultiTabView()
+        }
+    }
+}
+
